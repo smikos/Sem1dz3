78248a6db96fdbd2daac5876c0d1d85aeb0a7973
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-14 21:11:12 +0300

    work

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число =>");
+string inputedString = Console.ReadLine();  
+int number = Convert.ToInt32(inputedString);
+int number12 = (number%2);
+if (number12 == 0) Console.Write("DA");   
+else 
+Console.Write("no");
